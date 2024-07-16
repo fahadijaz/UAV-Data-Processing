50485233a49a3d3c1a14a0a618f0cc0e00e18a96
isakvg <isak.vg@gmail.com>
2024-07-16 09:52:20 +0200

    collected all data from old flights

--- collect_data_from_flights.py
+++ collect_data_from_flights.py
@@ -15,18 +15,19 @@ folder_list = os.listdir(input_path) #main folder
 print(folder_list)
 not_logged_flights = []
 for field in folder_list:
-    if field != '_TRASHCAN' and field != 'skyline':
-        all_folder_path = os.path.join(input_path,field,'MS')
+    if field != '_TRASHCAN' and field != 'skyline' and field != 'PILOT':
+        all_folder_path = os.path.join(input_path,field,'3D')
         for folder in os.listdir(all_folder_path):
             try:
-                folder_path = os.path.join(all_folder_path, folder)
-                folder_obj = FileTransfer(folder_path,output_path,data_overview_file=data_file, flight_log=flight_log)
-                folder_obj.get_information()
-                folder_obj.test_folder_for_TIF()
-                folder_obj.reflectance_types()
-                folder_obj.detect_and_handle_new_routes()
-                folder_obj.match()
-                folder_obj.save_flight_log()
+                if folder != 'Thumb.db':
+                    folder_path = os.path.join(all_folder_path, folder)
+                    folder_obj = FileTransfer(folder_path,output_path,data_overview_file=data_file, flight_log=flight_log)
+                    folder_obj.get_information()
+                    folder_obj.test_folder_for_TIF()
+                    folder_obj.reflectance_types()
+                    folder_obj.detect_and_handle_new_routes()
+                    folder_obj.match()
+                    folder_obj.save_flight_log()
             except Exception as e:
                 not_logged_flights.append([folder,folder_path])
 
