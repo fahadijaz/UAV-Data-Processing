99265acb9e194ed8605f78293519a5dff0bfe009
isakvg <isak.vg@gmail.com>
2024-07-25 11:42:39 +0200

    updated streamlit add flight page

--- St_add_flights.py
+++ St_add_flights.py
@@ -4,6 +4,10 @@ import os
 
 st.set_page_config(layout="wide")
 
+def look_for_sd_cards():
+    sd_card_paths = ["G:/DCIM", "I:/DCIM", "D:/DCIM"]
+    return [path for path in sd_card_paths if os.path.exists(path)]
+
 def load_file_transfers():
     st.session_state.current_index = 0
     st.session_state.file_transfers = []
@@ -14,8 +18,7 @@ def load_file_transfers():
     output_path = "P:\\PhenoCrop\\1_flights"
     data_file = "P:\\PhenoCrop\\0_csv\\flight_routes.csv"
     flight_log = "P:\\PhenoCrop\\0_csv\\flight_log.csv"
-    sd_card_paths = ["G:/DCIM", "I:/DCIM", "D:/DCIM"]
-    sd_card_paths = [path for path in sd_card_paths if os.path.exists(path)]
+    sd_card_paths = look_for_sd_cards()
     obj_list = [FileTransfer(input_path=path, output_path=output_path, data_overview_file=data_file, flight_log=flight_log) for path in sd_card_paths]
     for ft in obj_list:
         ft.get_information()
@@ -45,10 +48,14 @@ def display_file_transfers():
             if st.button('Edit this SD Card'):
                 st.session_state.edit_mode = True  # Toggle edit mode on
                 st.rerun()
-
-            if st.button('Next SD Card', on_click=lambda: next_sd_card()):
-                st.rerun()
-
+            if st.session_state.current_index < (len(st.session_state.file_transfers)-1):
+                if st.button('Next SD Card', on_click=lambda: next_sd_card()):
+                    st.rerun()
+            if st.session_state.current_index>0:
+                if st.button('Previous SD Card', on_click=lambda: prev_sd_card()):
+                    st.rerun()
+def prev_sd_card():
+    st.session_state.current_index -= 1
 def next_sd_card():
     """Move to the next SD card in the list, if available."""
     if st.session_state.current_index < len(st.session_state.file_transfers) - 1:
@@ -71,7 +78,15 @@ def edit_current_obj():
             col1, col2, col3, col4, col5 = st.columns([1,1.5,1.5,4,8])
             with col1:
                 if st.button("move"):
-                    pass
+                    col01, col02 = st.columns(2)
+                    with col01:
+                        flight_index = st.number_input('From:', min_value=0)
+                    with col02:
+                        new_path_index = st.number_input('To:',min_value=0)
+                    if st.button('confirm'):
+                        ft._move_path(streamlit_mode=True, flight_index=int(flight_index),new_path_index=int(new_path_index))
+                        st.rerun()
+                
             with col2:
                 if st.button("trash a folder"):
                     pass
@@ -91,6 +106,7 @@ def main():
     
     # Button to load SD cards
     if st.button("Load SD Cards"):
+        
         st.session_state.file_transfers = load_file_transfers()
         st.session_state.data_loaded = True
         st.session_state.ready_to_move = False  # Reset the move flag
@@ -104,7 +120,7 @@ def main():
             edit_current_obj()
         # Button to confirm all edits are done and ready to move files
         if st.session_state.file_transfers and not st.session_state.ready_to_move and st.session_state.current_index == (len(st.session_state.file_transfers)-1) and not st.session_state.edit_mode:
-            if st.button("Confirm All Edits"):
+            if st.button("Confirm"):
                 st.session_state.ready_to_move = True
                 st.success("Ready to move files. Please proceed with the final operation.")
         
