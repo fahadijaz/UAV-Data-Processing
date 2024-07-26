c47ca6d8552cb7abf32dbe1d1080e99de7ecf559
Sindre Stokke <sindrestokke@gmail.com>
2024-07-26 13:22:16 +0200

    Link button for flight images

--- St_flight_details.py
+++ St_flight_details.py
@@ -2,6 +2,9 @@ import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 from modules.flight_log_preprocessing import preprocessing
+import subprocess
+import os
+import sys
 
 #st.set_page_config(layout="wide")
 current_flight_ID = st.query_params["Index"]
@@ -11,7 +14,7 @@ df_flight_log, df_flight_routes, df_fields, df_flight_log_merged = preprocessing
 df_flight_log_merged['Index'] = range(1, len(df_flight_log_merged) + 1) # Creating a column telling whether the flight is the first, second, third, etc...
 
 flight_details = df_flight_log_merged[df_flight_log_merged["flight_ID"] == current_flight_ID].iloc[0]
-#flight_details
+flight_details
 
 title_col_1, title_col_2 = st.columns([0.88,0.12])
 with title_col_1:
@@ -34,28 +37,45 @@ body_column_1_content = ["image_type_keyword", "drone", "drone_pilot", "CameraAn
 
 body_column_2_content = ["LongName", "ResearchHead", "Researcher", "VollebekkResponsible", "Location", "Crop", "Varieties", "Plots", "Length", "Width", "NitrogenLevels", "FlightFrequency"]
 
+# Function to open the folder in the file explorer
+def open_folder(path):
+    if sys.platform == 'win32':
+        os.startfile(path)
+    elif sys.platform == 'darwin':
+        subprocess.Popen(['open', path])
+    else:
+        subprocess.Popen(['xdg-open', path])
+
 if (edit_mode == "📖"):
     #with body_column_1:
     #    flight_details_col_1 = flight_details[body_column_1_content]
     #    st.table(flight_details_col_1)
 
     with body_column_1:
-        st.write("&nbsp; Flight")
+        st.write("#### Flight")
         flight_details_col_1 = flight_details[body_column_1_content]
         st.table(flight_details_col_1)
+        # Button to open the folder
+        link_path = rf'{flight_details["output_path"]}'
+        #link_path = rf'P:\PhenoCrop\1_flights\{flight_details["Field ID"]}\{flight_details["BaseType"]}'
+        if st.button('Go to image folder'):
+            open_folder(link_path)
+            #st.write(f"Opened folder: {link_path}")
 
     with body_column_2:
-        st.write("&nbsp; Field")
+        st.write("#### Field")
         flight_details_col_2 = flight_details[body_column_2_content]
         st.table(flight_details_col_2)
 
 if (edit_mode == "✍"):
     with body_column_1:
-        st.write("&nbsp; Flight")
+        st.write("#### Flight")
         flight_details_col_1 = flight_details[body_column_1_content]
         st.data_editor(flight_details_col_1)
 
     with body_column_2:
-        st.write("&nbsp; Field")
+        st.write("#### Field")
         flight_details_col_2 = flight_details[body_column_2_content]
         st.table(flight_details_col_2)
+
+st.write("#### Processing status")
