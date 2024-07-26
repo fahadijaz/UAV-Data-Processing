3131df013ce6e1c56d78f9db67b26f2e8b53fdba
Sindre Stokke <sindrestokke@gmail.com>
2024-07-26 11:22:57 +0200

    Worked on edit functionality in St_flight_details. and Made date filtering work in St_review_flights.py

--- St_flight_details.py
+++ St_flight_details.py
@@ -13,7 +13,7 @@ df_flight_log_merged['Index'] = range(1, len(df_flight_log_merged) + 1) # Creati
 flight_details = df_flight_log_merged[df_flight_log_merged["flight_ID"] == current_flight_ID].iloc[0]
 #flight_details
 
-title_col_1, title_col_2 = st.columns([0.9,0.1])
+title_col_1, title_col_2 = st.columns([0.88,0.12])
 with title_col_1:
     st.markdown(f"""
     # Nr {flight_details["Index"]}: &nbsp;{flight_details["date"]} &nbsp; {flight_details["Field ID"]}&nbsp; {flight_details["start_time"]} - {flight_details["end_time"]}""")
@@ -21,7 +21,8 @@ with title_col_1:
 with title_col_2:
     st.text('')
     st.text('')
-    edit_mode = st.checkbox("✍")
+    #edit_mode = st.checkbox("✍")
+    edit_mode = st.selectbox("Route Type", ["📖","✍"], label_visibility="collapsed")
 
 body_column_1, body_column_2 = st.columns(2)
 body_column_3, body_column_4 = st.columns(2)
@@ -29,20 +30,32 @@ body_column_3, body_column_4 = st.columns(2)
 #body_column_1_content = ["Step 1 Quality Check", "Quality checked by", "Workable Data", "Ready for next step (step 2 and 3)", "Ready for next step - person",
 #                         "Step 2 processing", "Step 3 Image Stitching", "Processed by", "QGIS", "QGIS person"]
 
-body_column_1_content = ["image_type_keyword", "drone", "CameraAngle", "Speed", "height", "BaseOverlap", "start_time", "end_time", "num_files", "num_dir"]
+body_column_1_content = ["image_type_keyword", "drone", "drone_pilot", "CameraAngle", "Speed", "height", "BaseOverlap", "start_time", "end_time", "num_files", "num_dir"]
 
 body_column_2_content = ["LongName", "ResearchHead", "Researcher", "VollebekkResponsible", "Location", "Crop", "Varieties", "Plots", "Length", "Width", "NitrogenLevels", "FlightFrequency"]
 
-#with body_column_1:
-#    flight_details_col_1 = flight_details[body_column_1_content]
-#    st.table(flight_details_col_1)
+if (edit_mode == "📖"):
+    #with body_column_1:
+    #    flight_details_col_1 = flight_details[body_column_1_content]
+    #    st.table(flight_details_col_1)
 
-with body_column_1:
-    st.write("&nbsp; Flight")
-    flight_details_col_1 = flight_details[body_column_1_content]
-    st.table(flight_details_col_1)
+    with body_column_1:
+        st.write("&nbsp; Flight")
+        flight_details_col_1 = flight_details[body_column_1_content]
+        st.table(flight_details_col_1)
 
-with body_column_2:
-    st.write("&nbsp; Field")
-    flight_details_col_2 = flight_details[body_column_2_content]
-    st.table(flight_details_col_2)
+    with body_column_2:
+        st.write("&nbsp; Field")
+        flight_details_col_2 = flight_details[body_column_2_content]
+        st.table(flight_details_col_2)
+
+if (edit_mode == "✍"):
+    with body_column_1:
+        st.write("&nbsp; Flight")
+        flight_details_col_1 = flight_details[body_column_1_content]
+        st.data_editor(flight_details_col_1)
+
+    with body_column_2:
+        st.write("&nbsp; Field")
+        flight_details_col_2 = flight_details[body_column_2_content]
+        st.table(flight_details_col_2)
