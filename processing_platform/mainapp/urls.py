7ba362790bb8223c921ee8621ce8001d861d4817
fredericstrand <strandfrederic@outlook.com>
2025-06-30 18:32:55 +0200

    working sd card and weekly overview

--- processing_platform/mainapp/urls.py
+++ processing_platform/mainapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('sd-card/', views.sd_card_view, name='sd_card'),
-    
-    
+    path('weekly/', views.weekly_view, name='weekly'),
+    path('details/', views.details_view, name='details'),
+    path('add-routes/', views.add_routes_view, name='add_routes'),
 ]
