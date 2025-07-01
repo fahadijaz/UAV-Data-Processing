72ed1147c94fdf0852f04c428bb1e8c4ec6f87eb
fredericstrand <strandfrederic@outlook.com>
2025-07-01 10:20:48 +0200

    updated weekly overview

--- processing_platform/mainapp/urls.py
+++ processing_platform/mainapp/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('sd-card/', views.sd_card_view, name='sd_card'),
     path('weekly/', views.weekly_view, name='weekly'),
+    re_path(r"^weekly/(?P<week_offset>-?\d+)/$", views.weekly_view, name="weekly_with_offset"),
     path('details/', views.details_view, name='details'),
     path('add-routes/', views.add_routes_view, name='add_routes'),
 ]
