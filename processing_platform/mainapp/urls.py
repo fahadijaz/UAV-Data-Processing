492c8fdbb2d8cad5dfc2d853460b9c89cf054766
chris2pher1503 <christopher150304@outlook.com>
2025-07-02 15:40:24 +0200

    feat: implemented, but missing api

--- processing_platform/mainapp/urls.py
+++ processing_platform/mainapp/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
-    path('sd-card/', views.sd_card_view, name='sd_card'),
-    path('review_drone_flights/', views.review_drone_flights, name='review_drone_flights'),
+    path("", views.home_view, name="home"),
+    path("sd-card/", views.sd_card_view, name="sd_card"),
+    path(
+        "review_drone_flights/", views.review_drone_flights, name="review_drone_flights"
+    ),
     path("read-csv/", views.read_local_csv),
-    path('weekly/', views.weekly_view, name='weekly'),
-    path('details/', views.details_view, name='details'),
-    path('add-routes/', views.add_routes_view, name='add_routes'),
+    path("weekly/", views.weekly_view, name="weekly"),
+    path("details/", views.details_view, name="details"),
+    path("add-routes/", views.add_routes_view, name="add_routes"),
+    path("easy_growth/", views.easy_growth_view, name="easy_growth"),
 ]
